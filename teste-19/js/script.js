bfe59d44e8ed4e0307cde876117b0517a73ffb7c
Luc4s G4br13l P3r31r4 d4 Cunh4 <91632301+lucas0456@users.noreply.github.com>
2022-12-29 16:14:54 -0300

    Atualização do dia 29/12/2022
    Validação

---
+++ teste-19/js/script.js
@@ -0,0 +1,25 @@
+function validation() {
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.match(pattern)){
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Belo email hein";
+        text.style.color = "#00ff00";
+    }
+    else {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "Ensira direto";
+        text.style.color = "#ff0000";
+    }
+    if(email == ""){
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "";
+        text.style.color = "#00ff00";
+    }
+}
