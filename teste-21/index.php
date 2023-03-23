dbd68e28584989fad63123344d9d8b85a0562b2f
Alexandre-Eteot <valadaomilene@gmail.com>
2023-03-23 10:38:36 -0300

    Atualização do dia 23/03/2023

--- teste-21/index.php
+++ teste-21/index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>card</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <div class="container-fluid p3">
-    <div class="row">
-      <div class="col text-start">
-    <h1>Minha loja</h1>
-  </div>
-    <div class="col text-end">
-      <button class="btn btn-primary me-md-2" type="button">Button</button>
-      <button class="btn btn-primary" type="button">Button</button>
-    </div>
-    </div>
-    <br>
-    
-<div class="container">
+<?php
+include "topo.php";
+?>
   <div class="row">
     <div class="col-12 col-md-4 col-lg-3 mt-5">
         <img src="../teste-21/img/download.jpg" class="img-fluid mb3" alt="">
@@ -114,23 +93,6 @@
       </div>
     </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
-
-  <hr>
-  <div class="container mt-5 mb-5">
-    <div class="row">
-      <div class="col-12 col-md-6 col-lg-2 text-start">
-        <input type="text" class="form-control" placeholder="Buscar">
-      </div>
-      <div class="col-12 col-md-1 text-start">
-        <button type="button" class="btn btn-primary">Submit</button>
-      </div>
-      <div class="col col-md-6 text-start">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim fugit hic deleniti illo cum consequatur repudiandae exercitationem aliquid</p>
-      </div>
-      <div class="col col-md-3 text-end"></div>
-    </div>
-  </div>
-</body>
-</html>
+<?php
+include "rodape.php";
+?>
